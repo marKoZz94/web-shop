0d915cbfddff38eca67a08dac6d961146e759184
marKoZz94 <35594819+marKoZz94@users.noreply.github.com>
2022-12-11 19:21:10 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# dbnip_front
+## Upustvo 
+
+Potrebno je imati instaliran node 14.7.0, npm je onda 6.14.7
+
+Preporuka da se koristi NVM, pa da mozes da menjas verzije
+
+npm install, obavezno
---
+++ decs.d.ts
@@ -0,0 +1,16 @@
+declare module "react-simple-chatbot";
+declare module "react-render-html";
+declare module "query-string";
+declare module "mic-recorder-to-mp3";
+declare module "react-ratings-declarative";
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.tiff';
+declare module "react-expand-animated";
+declare module "swiper";
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.svg";
+declare module 'react-file-viewer'
+declare module 'react-file-viewer-extended'
+//declare module "wavesurfer-react";
